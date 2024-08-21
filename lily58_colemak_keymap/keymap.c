b60667698e2150b828f00a8cd504b411a1b88d70
Markus Rapp Nyland <git@mrn.com>
2024-08-21 18:51:55 +0200

    Switch keys and adjust ascii

--- lily58_colemak_keymap/keymap.c
+++ lily58_colemak_keymap/keymap.c
@@ -9,22 +9,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* COLEMAK (actually colemak-dh)
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * | ESC  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  = / +  |
+ * | ESC  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |   |  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | Tab  |   Q  |   W  |   F  |   P  |   B  |                    |   J  |   L  |   U  |   Y  |   Ø  |  - | &  |
+ * | Tab  |   Q  |   W  |   F  |   P  |   B  |                    |   J  |   L  |   U  |   Y  |   Ø  |   =  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | BcSp |   A  |   R  |   S  |   T  |   G  |-------.    ,-------|   M  |   N  |   E  |   I  |   O  |  '   |
- * |------+------+------+------+------+------|   `   |    |   \   |------+------+------+------+------+------|
- * |   [  |   Z  |   X  |   C  |   D  |   V  |-------|    |-------|   K  |   H  |   ,  |   .  |   / - _  |  ]   |
- * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LAlt | LGUI | LCTL | /LShift /       \ NAVI \  | RGUI |  RCTL  | RAlt / |
- *                   |      |      |      |/[Space]/         \[Entr]\ |      |        | AltGR  |
- *                   `----------------------------'           '------''--------------------'
+ * | BcSp |   A  |   R  |   S  |   T  |   G  |-------.    ,-------|   M  |   N  |   E  |   I  |   O  |   '  |
+ * |------+------+------+------+------+------|   `   |    |   ~   |------+------+------+------+------+------|
+ * |   [  |   Z  |   X  |   C  |   D  |   V  |-------|    |-------|   K  |   H  |   ,  |   .  |   -  |   ]  |
+ * `-----------------------------------------'       /     \      '-----------------------------------------'
+ *                   | LAlt | LGUI | LCTL |   Spc   /       \   Ent   | RAlt | RGUI | RCTL |
+ *                   |      |      |      | LShift /         \  NAVI  |      |      |      |
+ *                   '----------------------------'           '----------------------------'
  */
 
 [_COLEMAK] = LAYOUT(
-   KC_ESC, KC_1, KC_2,    KC_3,    KC_4,       KC_5,                                    KC_6,      KC_7,            KC_8,    KC_9,    KC_0,    KC_EQL,
-   KC_TAB, KC_Q, KC_W,    KC_F,    KC_P,       KC_B,                                    KC_J,      KC_L,            KC_U,    KC_Y,    KC_SCLN, KC_MINS,
+   KC_ESC, KC_1, KC_2,    KC_3,    KC_4,       KC_5,                                    KC_6,      KC_7,            KC_8,    KC_9,    KC_0,    KC_MINS,
+   KC_TAB, KC_Q, KC_W,    KC_F,    KC_P,       KC_B,                                    KC_J,      KC_L,            KC_U,    KC_Y,    KC_SCLN, KC_EQL,
   KC_BSPC, KC_A, KC_R,    KC_S,    KC_T,       KC_G,                                    KC_M,      KC_N,            KC_E,    KC_I,    KC_O,    KC_QUOT,
   KC_LBRC, KC_Z, KC_X,    KC_C,    KC_D,       KC_V,         KC_GRV,          KC_BSLS,  KC_K,      KC_H,            KC_COMM, KC_DOT, KC_SLSH, KC_RBRC,
                        KC_LALT, KC_LGUI,    KC_LCTL, LSFT_T(KC_SPC), LT(_NAVI,KC_ENT), KC_RALT, KC_RGUI, KC_RCTL
