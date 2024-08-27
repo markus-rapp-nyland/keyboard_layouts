fb0bf4b12a37affb7c7c9c01571dae45c02726d0
Markus Rapp Nyland <git@mrn.com>
2024-08-27 15:36:12 +0200

    Adjust setup to free up the ø key

--- lily58_colemak_keymap/keymap.c
+++ lily58_colemak_keymap/keymap.c
@@ -11,7 +11,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * | ESC  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |   |  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | Tab  |   Q  |   W  |   F  |   P  |   B  |                    |   J  |   L  |   U  |   Y  |   Ø  |   =  |
+ * | Tab  |   Q  |   W  |   F  |   P  |   B  |                    |   J  |   L  |   U  |   Y  |   =  |   /  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | BcSp |   A  |   R  |   S  |   T  |   G  |-------.    ,-------|   M  |   N  |   E  |   I  |   O  |   '  |
  * |------+------+------+------+------+------|   `   |    |   ~   |------+------+------+------+------+------|
@@ -23,9 +23,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_COLEMAK] = LAYOUT(
-   KC_ESC, KC_1, KC_2,    KC_3,    KC_4,       KC_5,                                    KC_6,      KC_7,            KC_8,    KC_9,    KC_0,    KC_MINS,
-   KC_TAB, KC_Q, KC_W,    KC_F,    KC_P,       KC_B,                                    KC_J,      KC_L,            KC_U,    KC_Y,    KC_SCLN, KC_EQL,
-  KC_BSPC, KC_A, KC_R,    KC_S,    KC_T,       KC_G,                                    KC_M,      KC_N,            KC_E,    KC_I,    KC_O,    KC_QUOT,
+   KC_ESC, KC_1, KC_2,    KC_3,    KC_4,       KC_5,                                    KC_6,      KC_7,            KC_8,    KC_9,     KC_0,  KC_MINS,
+   KC_TAB, KC_Q, KC_W,    KC_F,    KC_P,       KC_B,                                    KC_J,      KC_L,            KC_U,    KC_Y,   KC_EQL,  KC_SCLN,
+  KC_BSPC, KC_A, KC_R,    KC_S,    KC_T,       KC_G,                                    KC_M,      KC_N,            KC_E,    KC_I,     KC_O,  KC_QUOT,
   KC_LBRC, KC_Z, KC_X,    KC_C,    KC_D,       KC_V,         KC_GRV,          KC_BSLS,  KC_K,      KC_H,            KC_COMM, KC_DOT, KC_SLSH, KC_RBRC,
                        KC_LCTL, KC_LGUI,    KC_LALT, LSFT_T(KC_SPC), LT(_NAVI,KC_ENT), KC_RALT, KC_RGUI, KC_RCTL
 ),
